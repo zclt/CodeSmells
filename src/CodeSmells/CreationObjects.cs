b67d6e6803c8d0c5bb20c2c41e5bad3703a3b93c
matheus.pereira <System33a!>
2020-01-07 15:17:17 -0300

    Creation

---
+++ src/CodeSmells/CreationObjects.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeSmells
+{
+    public static class CreationObjects
+    {
+        public static Foo CreateNewFoo()
+        {
+            var foo = new Foo() { Code = 1 };
+            return foo;
+        }
+    }
+    
+    public class Foo 
+    {
+        public int Code { get; set; }
+    }
+}
